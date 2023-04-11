205ca5ea1a611a03fff72e299700aa3fd54ab40d
Mahesh Dange <maheshd212000@gmail.com>
2023-04-11 10:05:45 +0530

    removed unnecessary comments

--- app.js
+++ app.js
@@ -15,45 +15,6 @@ app.get("/", function(req, res){
     let currentDay = today.getDay() ;
     var day = "" ;
 
-    // switch (currentDay){
-    //     case 0 :
-    //         day = "Sunday" ;
-    //         break ;
-
-    //     case 1 :
-    //         day = "Monday" ;
-    //         break ;            
-
-    //     case 2 :
-    //         day = "Tuesday" ;
-    //         break ;            
-
-    //     case 3 :
-    //         day = "Wednesday" ;
-    //         break ;
-
-    //     case 4 :
-    //         day = "Thrusday" ;
-    //         break ;            
-
-    //     case 5 :
-    //         day = "Friday" ;
-    //         break ;      
-        
-    //     case 6 :
-    //         day = "Saturday" ;
-    //         break ;
-        
-    //     default:
-    //     console.log("Error " + currentDay);
-    // }
-    
-    // if( currentDay === 6 || currentDay === 0){
-    //     day = "Weekend " ;
-    // }
-    // else{
-    //     day = "Weekday " ; 
-    // }
     
     let options = {
         weekday: "long" ,
